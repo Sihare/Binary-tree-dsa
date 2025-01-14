458f09cf2b7d044b1b7cbb8200a2b79d1e72de51
Nikhil sihare <96694987+Sihare@users.noreply.github.com>
2025-01-14 18:41:47 +0530

    Time: 3 ms (75.81%) | Memory: 85.7 MB (85.75%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-         int n = A.size();
-        unordered_map<int, int>mp;
-        vector<int>ans(n, 0);
-        for (int i = 0; i < n; i++)
-        {
-            mp[B[i]]++;
-        
-            int common = 0;
-            for (int j = 0; j <= i; j++)
-            {
-                if (mp.count(A[j])) common++;
+         int n=A.size();
+         vector<int>count(n+1,0);
+         vector<int>ans(n);
+    int c=0;
+         for(int i=0;i<n;i++){
+            count[A[i]]++;//yaha pr hum logo ne A array ka count rakha
+            if(count[A[i]]==2){// Yeh permutation hai toh sirf 2 hii max
+            //frequency ho sakti hai
+                c++;
             }
-           
-            ans[i] = common;
-        }
-        return ans;
+            count[B[i]]++;// Same as A
+            if(count[B[i]]==2){
+                c++;
+            }
+            ans[i]=c;
+         }
+         return ans;
         
     }
 };
